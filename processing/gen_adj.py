a81e4945d506c64a99f1d74a29d26c21010b4be1
Zhuzhihua <zhuzhihua@ict.ac.cn>
2020-11-05 14:55:10 +0800

    processing

---
+++ processing/gen_adj.py
@@ -0,0 +1,58 @@
+import scipy.sparse as sp
+import numpy as np
+
+def normt_spm(mx, method='in'):
+    if method == 'in':
+        # mx = mx.transpose()
+        rowsum = np.array(mx.sum(1))
+        r_inv = np.power(rowsum, -1).flatten()
+        r_inv[np.isinf(r_inv)] = 0.
+        r_mat_inv = sp.diags(r_inv)
+        mx = r_mat_inv.dot(mx)
+        return mx
+    if method == 'sym':
+        rowsum = np.array(mx.sum(1))
+        r_inv = np.power(rowsum, -0.5).flatten()
+        r_inv[np.isinf(r_inv)] = 0.
+        r_mat_inv = sp.diags(r_inv)
+        mx = mx.dot(r_mat_inv).transpose().dot(r_mat_inv)
+        return mx
+
+
+def generate_adj(edge_type):
+    dir = 'your data path'
+    # "%s.txt" is the edges' file. e.g., PA.txt: <P_node_idx>\t<A_node_idx>
+    path = dir + '%s.txt' % (edge_type)
+
+    edges = np.loadtxt(path, delimiter='\t', dtype='int')
+
+    # "%s.txt" is the nodes' file. e.g., P.txt: <P_node_idx>\t<node_name>
+    with open(dir + '%s.txt' % edge_type[0], 'r') as lfile:
+        l_node_num = len(lfile.readlines())
+    with open(dir + '%s.txt' % edge_type[1], 'r') as rfile:
+        r_node_num = len(rfile.readlines())
+
+    row = l_node_num
+    col = r_node_num
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1] + row)),
+                        shape=(row+col, row+col),
+                        dtype='float32'
+                        )
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    L_adj = normt_spm(adj, method='sym')
+
+    L_adj = sp.coo_matrix(L_adj)
+
+
+    L_adj = L_adj.todense()
+
+    L_adj_h = np.hsplit(L_adj, (row,))[1]
+    L_adj_v = np.vsplit(L_adj_h, (row,))[0]
+    sp.save_npz('./%s.adj.npz' % edge_type, sp.coo_matrix(L_adj_v))
+
+
+if __name__ == '__main__':
+    edge_types = [""]
+    for edge_type in edge_types:
+        generate_adj(edge_type)
---
+++ processing/gen_feat.py
@@ -0,0 +1,78 @@
+import numpy as np
+import scipy.sparse as sp
+import argparse
+import random
+
+nodes_map = {
+    'node type': 0,
+}
+
+def read_dat(path, file_name):
+    path = path + file_name
+
+    data = np.loadtxt(path, delimiter='\t', dtype=np.int)
+
+    return data
+
+def encode_onehot(labels, flag=False):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+
+    if not flag:
+        return classes_dict
+
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+def gen_labels(target_type, ispart=True):
+    # the label file: <node_idx>\t<label_idx>. if <node_idx> has not a label, <label_idx> is fixed as 0,
+    labels = read_dat(path='your data path', file_name='label file')
+
+    labels_part = []
+    for i, j in enumerate(labels):
+        if ispart:
+            if j != 0:
+                labels_part.append([i, j])
+        else:
+            labels_part.append([i, j])
+
+    with open('./%s.label.all' % target_type, 'w') as fall:
+        for l in labels:
+            fall.write('{}\n'.format(l))
+
+    with open('./%s.label.part' % target_type, 'w') as fpart:
+        for l in labels_part:
+            fpart.write('{}\t{}\n'.format(l[0], l[1]))
+
+def label_feature(path, output_path, node_type, target_type):
+    labels = np.loadtxt(path, delimiter='\t', dtype=np.dtype(str))
+    class_num = len(set(list(labels))) - 1
+
+    if node_type == target_type:
+
+        feat = encode_onehot(labels, flag=True)
+        feat = feat[:, 1:]
+        feat = sp.csr_matrix(feat)
+    else:
+        entity_n = nodes_map[node_type]
+        sample_n = class_num
+        feat = sp.csr_matrix(np.zeros([entity_n, sample_n]))
+
+    sp.save_npz(output_path, feat)
+
+if __name__ == '__main__':
+    target_type = ''
+
+    gen_labels(target_type=target_type)
+
+
+    node_types = ['']
+    for node_type in node_types:
+        label_feature(path='./%s.label.all' % target_type,
+                        output_path='./%s.feat.label' % node_type,
+                        node_type=node_type,
+                      target_type=target_type)
+
+
